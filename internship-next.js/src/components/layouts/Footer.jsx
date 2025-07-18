a184b0875053e91cbe69239ff618e36cffb79325
liliya-freckle <liliya.nagrimanova@gmail.com>
2025-07-19 03:23:05 +0500

    chech commit

--- internship-next.js/src/components/layouts/Footer.jsx
+++ internship-next.js/src/components/layouts/Footer.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className="bg-slate-400 shadow-md h-9 mt-8">
-        <div className='text-center align-bottom text-l'>
-            @database 2025
+    <footer className="bg-black shadow-md h-9 mt-8 content-center">
+        <div className='text-center align-center text-l text-white'>
+            ©database 2025
         </div>
     </footer>
   )
--- internship-next.js/src/components/layouts/Header.jsx
+++ internship-next.js/src/components/layouts/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <header  className="bg-slate-400 shadow-md h-12 pr-9 mb-8">
+    <header  className="bg-blue-500 shadow-md h-14 pr-9 pt-3 mb-9">
       <div className="text-right align-middle font-extrabold text-white/90 text-2xl "> 
         You-search.com
       </div>
