df6f2a438370dbd18499fe0a402042f4092e3db3
liliya-freckle <liliya.nagrimanova@gmail.com>
2025-07-19 03:31:19 +0500

    removed a folder

---
+++ src/components/layouts/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className="bg-black shadow-md h-9 mt-8 content-center">
+        <div className='text-center align-center text-l text-white'>
+            ©database 2025
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
